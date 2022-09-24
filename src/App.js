299b5b0f1347c695c9519c4f3c62371a7af7c44d
Andrea Eurrieta <deyeurrieta@gmail.com>
2022-09-24 15:21:53 -0300

    arreglo conflictos

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
 // import RegisterForm from "./components/auth/RegisterForm";
 import './App.css'
-<<<<<<< HEAD
 import Header from './components/Header/Header';
 import { PageP } from './components/Page/PageP';
-
-function App() {
-  return (
-    <PageP/>
-=======
 import LoginForm from "./components/auth/LoginForm";
 
 function App() {
@@ -15,9 +9,8 @@ function App() {
     <div className="App">
        <LoginForm/>
        <RegisterForm/>
-      ccss
+       <PageP/>
     </div>
->>>>>>> 1224c0975bd66533ce17a138ebd28bde69786883
   );
 }
 
